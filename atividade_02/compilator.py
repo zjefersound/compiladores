abe64c2ec7fac3ac0b48ee68ea7893a20c8a54d8
Jeferson Santos Moraes de Souza <62676057+zjefersound@users.noreply.github.com>
2025-03-14 23:50:15 +0000

    Atividade 2 - Exercício 03

--- atividade_02/compilator.py
+++ atividade_02/compilator.py
@@ -16,6 +16,18 @@ def extract_ascii_characters(file_path):
 def filter_unwanted_characters(char_list, unwanted_chars):
     return [char for char in char_list if char not in unwanted_chars]
 
+def get_numbered_lines(file_path):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            lines = file.readlines()
+            max_digits = len(str(len(lines)))
+            return [f"{str(i+1).rjust(max_digits)} | {line.rstrip()}" for i, line in enumerate(lines)]
+    except FileNotFoundError:
+        print("Arquivo não encontrado.")
+    except Exception as e:
+        print(f"Ocorreu um erro: {e}")
+
+
 # Vai para uma camada de input
 file_name = "our_strange_code.txt"
 file_path = os.path.join(os.path.dirname(__file__), file_name)
@@ -30,3 +42,10 @@ filtered_characters = filter_unwanted_characters(ascii_characters, unwanted_char
 if filtered_characters:
     print("Caracteres ASCII extraídos:")
     print(filtered_characters)
+
+
+numbered_lines = get_numbered_lines(file_path)
+if numbered_lines:
+    print("\nTexto com numeração de linhas:")
+    for line in numbered_lines:
+        print(line)
